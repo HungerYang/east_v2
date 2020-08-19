8f8b7e3866f0f45035d51eb3a30248517ecc5295
yangxuehang <yangxuehang001@163.com>
2020-08-20 01:14:06 +0800

    first

--- demo.sh
+++
@@ -1,7 +0,0 @@
-python demo_pb.py \
---test_data_path=./test_data/ \
---text_scale=512 \
---gpu_list=3 \
---pb_path=./ckpts/earlyPB/east_grid_book_general.pb  \
---output_dir=result/tmp6/
-
--- test.sh
+++
@@ -1,7 +0,0 @@
-python predict.py \
---test_data_path=./test_data/ \
---text_scale=512 \
---gpu_list=2 \
---checkpoint_path=./ckpts/model-base/ \
---output_dir=./result/tmp3/
-
